6dbdd2728dd5993693418138e321d3a5e9a90281
Kat Syddall <kat.syddall@gmail.com>
2024-05-16 14:12:30 +0200

    implemented required backend

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,5 @@
 class Movie < ApplicationRecord
   has_many :bookmarks
-  has_many :lists, through: :bookmarks # not sure if I need this...
 
   validates :title, presence: true, uniqueness: true
   validates :overview, presence: true
