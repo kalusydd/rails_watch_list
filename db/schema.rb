a3c66f6b424e6fae60d7dd5df09580bcd90cdb61
Kat Syddall <kat.syddall@gmail.com>
2024-06-05 17:33:07 +0200

    updated movie model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_04_155541) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_05_152811) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -39,6 +39,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_04_155541) do
     t.integer "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "director"
+    t.integer "year"
   end
 
   create_table "users", force: :cascade do |t|
